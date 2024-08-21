4ac564b287351a2913d7a35a7888616d34c52d78
Lucas Pearson <catdevman@gmail.com>
2024-08-21 19:53:17 -0400

    update docs from go generate

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     clickup = {
-        version = "~> 0.0.1"
-        source = "hashicorp.io/catdevman/clickup"
+      version = "~> 0.0.1"
+      source  = "hashicorp.io/catdevman/clickup"
     }
   }
 }
 
 variable "CLICKUP_API_KEY" {
-    type = string
+  type = string
 }
 
 provider "clickup" {
@@ -17,60 +17,60 @@ provider "clickup" {
 
 data "clickup_teams" "teams" {}
 
-data "clickup_usergroups" "groups"{}
+data "clickup_usergroups" "groups" {}
 
 data "clickup_spaces" "spaces" {
-    team_id = "9014024487"
+  team_id = "9014024487"
 }
 
 data "clickup_space" "space" {
-    space_id = "90140051562"
+  space_id = "90140051562"
 }
 
 data "clickup_folders" "folders" {
-    space_id = "90140051562"
+  space_id = "90140051562"
 }
 
 data "clickup_folder" "folder" {
-    folder_id = "90140096619"
+  folder_id = "90140096619"
 }
 
 data "clickup_lists" "lists" {
-    folder_id = "90140103670"
+  folder_id = "90140103670"
 }
 
-data "clickup_folderless_lists" "folderless_lists"{
-    space_id = data.clickup_space.space.space.id
+data "clickup_folderless_lists" "folderless_lists" {
+  space_id = data.clickup_space.space.space.id
 }
 
 output "outtie" {
-    value = data.clickup_teams.teams
+  value = data.clickup_teams.teams
 }
 
 output "outtie2" {
-    value = data.clickup_usergroups.groups
+  value = data.clickup_usergroups.groups
 }
 
 output "outtie3" {
-    value = data.clickup_spaces.spaces
+  value = data.clickup_spaces.spaces
 }
 
 output "outtie4" {
-    value = data.clickup_space.space
+  value = data.clickup_space.space
 }
 
 output "outtie5" {
-    value = data.clickup_folders.folders
+  value = data.clickup_folders.folders
 }
 
 output "outtie6" {
-    value = data.clickup_folder.folder
+  value = data.clickup_folder.folder
 }
 
 output "outtie7" {
-    value = data.clickup_lists.lists
+  value = data.clickup_lists.lists
 }
 
 output "outtie8" {
-    value = data.clickup_folderless_lists.folderless_lists
+  value = data.clickup_folderless_lists.folderless_lists
 }
